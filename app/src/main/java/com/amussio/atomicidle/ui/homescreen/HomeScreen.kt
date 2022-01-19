5436ad24c0a0ce4a7e9bd22e75246aee31fdedce
Adrien Mussio <adrien.mussio@withings.com>
2022-01-19 09:21:57 +0100

    add border

--- app/src/main/java/com/amussio/atomicidle/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/amussio/atomicidle/ui/homescreen/HomeScreen.kt
@@ -1,14 +1,16 @@
 package com.amussio.atomicidle.ui.homescreen
 
 import android.widget.Space
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,18 +23,16 @@ import androidx.compose.ui.unit.dp
 import com.amussio.atomicidle.data.extensions.printProduction
 import com.amussio.atomicidle.data.helpers.getMockStock
 import com.amussio.atomicidle.data.models.Element
+import com.amussio.atomicidle.ui.theme.*
+import kotlinx.coroutines.*
 
 @ExperimentalFoundationApi
 @Composable
 fun HomeScreen() {
     val stock = getMockStock()
     LazyVerticalGrid(
-        cells = GridCells.Adaptive(100.dp),
-        contentPadding = PaddingValues(
-            start = 12.dp,
-            top = 16.dp,
-            end = 12.dp,
-            bottom = 16.dp
+        cells = GridCells.Adaptive(Item1),
+        contentPadding = PaddingValues(vertical = VerticalPadding, horizontal = HorizontalPadding
         )) {
         items(stock.elements.count()) {
             ItemElement(stock.elements.get(it))
@@ -48,21 +48,34 @@ fun ItemGroup(name: String) {
 @Composable
 fun ItemElement(element: Element) {
     val context = LocalContext.current
+    var enable by remember { mutableStateOf(true) }
+    val scope by remember { mutableStateOf(CoroutineScope(Dispatchers.Default)) }
     Card(
         backgroundColor = Color.Black,
-        elevation = 1.dp,
-        modifier = Modifier.padding(4.dp)) {
+        border= if (enable) { BorderStroke(Border1, Color.White) } else { null }, //todo faire un truc avec ça
+        modifier = Modifier
+            .padding(Layout1)
+            .clickable {
+                if (enable == true) {
+                    enable = false
+                    element.quantity += 1 //faire un repository
+                    scope.launch {
+                        delay(1000L)
+                        enable = true
+                    }
+                }
+            }) {
         Box {
             val imageResource = context.resources.getIdentifier(element.name.lowercase(), "drawable", context.packageName)
             Image(painter = painterResource(id = imageResource), contentDescription = element.name)
             Column(modifier = Modifier
-                .padding(8.dp)
+                .padding(Layout2)
                 .align(alignment = Alignment.TopCenter)) {
                 Text(modifier = Modifier.fillMaxSize(), textAlign = TextAlign.Center, text = element.name)
-                Text(modifier = Modifier.fillMaxSize(), textAlign = TextAlign.Center, text = "x ${element.quantity}")
+                Text(modifier = Modifier.fillMaxSize(), textAlign = TextAlign.Center, text = "x ${element.quantity /*TODO faire un repository qui recupere la valeur*/}")
             }
             Text(modifier = Modifier
-                .padding(8.dp)
+                .padding(Layout2)
                 .align(alignment = Alignment.BottomCenter),text="+ ${element.printProduction()} /s")
 
         }
---
+++ app/src/main/java/com/amussio/atomicidle/ui/theme/Dimensions.kt
@@ -0,0 +1,13 @@
+package com.amussio.atomicidle.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val VerticalPadding = 16.dp
+val HorizontalPadding = 12.dp
+
+val Layout1 = 4.dp
+val Layout2 = 8.dp
+
+val Item1 = 100.dp
+
+val Border1 = 1.dp
